6d500a2471319dd5a46b815c21cb5e5c8a2301e2
w-boom <2506259430@qq.com>
2022-04-13 10:38:10 +0800

    add build procedure

---
+++ ProcessingProcedure/HelloWorld.bat
@@ -0,0 +1,1 @@
+gcc -E HelloWorld.c -o hello.i
---
+++ ProcessingProcedure/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+int main()
+{
+	printf("Hello World\n");
+	double pi = PI;
+	printf("%f", pi);
+	
+	return 0;
+}
