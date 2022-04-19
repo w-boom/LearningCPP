09effa4082fbded6a56e0042d3f711e2a17540de
w-boom <2506259430@qq.com>
2022-04-19 17:24:56 +0800

    对std::size_t的误解，其实是unsigned int

---
+++ BugStdSizeT.cpp
@@ -0,0 +1,12 @@
+// std::size_t
+// std::size_t is the unsigned integer type of the result of the sizeof operator
+#include <iostream>
+
+int main()
+{
+    std::size_t a = 3;
+    std::size_t b = 5;
+    std::cout << a - b << std::endl;
+    
+    return 0;
+}
