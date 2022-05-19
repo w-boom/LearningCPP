a61d3c1851d4e051ebe65171049d9ca23c2a3baa
w-boom <2506259430@qq.com>
2022-05-19 08:48:26 +0800

    补充float精度不够

--- C/Overflow.c
+++ C/Overflow.c
@@ -5,6 +5,7 @@ int main(void)
     /* int类型溢出会从-max开始 */
     /* unsigned int类型溢出会从0开始 */
 	/* 浮点类型溢出会显示inf */
+	
     int i = 2147483647;
     unsigned int j = 4294967295;
 	float toobig = 3.4E38 * 100.0f;
@@ -13,6 +14,12 @@ int main(void)
     printf("%u %u %u\n", j, j+1, j+2);
     printf("%e", toobig);
 	
-
+	/* * * 增加浮点数 * * */		
+	/*float a, b;
+	* b = 2.0e20 + 1.0;
+	* a = b - 2.0e20;
+	* printf("%f \n", b);
+	* printf("%f \n", a); */
+	
     return 0;
 }
