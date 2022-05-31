e4e5cf6cd2102b4cf1070b06a25771ff9fe00465
w-boom <2506259430@qq.com>
2022-05-31 17:27:17 +0800

    增加FreadFwrite.c fread & fwrite 都是处理二进制文件

---
+++ C/StandardIO/FreadFwrite.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+enum { SIZE = 5 };
+int main(void)
+{
+    double a[SIZE] = {1.,2.,3.,4.,5.};
+    FILE *fp = fopen("test.txt", "wb"); // must use binary mode
+    fwrite(a, sizeof *a, SIZE, fp); // writes an array of doubles
+    fclose(fp);
+ 
+    double b[SIZE];
+    fp = fopen("test.txt","rb");
+    size_t ret_code = fread(b, sizeof *b, SIZE, fp); // reads an array of doubles
+    if(ret_code == SIZE) {
+        puts("Array read successfully, contents: ");
+        for(int n = 0; n < SIZE; ++n) printf("%f ", b[n]);
+        putchar('\n');
+    } else { // error handling
+       if (feof(fp))
+          printf("Error reading test.txt: unexpected end of file\n");
+       else if (ferror(fp)) {
+           perror("Error reading test.txt");
+       }
+    }
+ 
+    fclose(fp);
+}
